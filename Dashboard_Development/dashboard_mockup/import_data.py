9b465f689d41b051fd396b083cba8d3122d548f6
Hagen E. Fritz <hagenfritz@gmail.com>
2019-08-22 10:01:18 -0500

    Created a migration to import the data

---
+++ Dashboard_Development/dashboard_mockup/import_data.py
@@ -0,0 +1,24 @@
+
+# Full path and name to your csv file
+csv_filepathname="/~/Google\ Drive/Research/5_Sensors/Projects/UT2000-SummerDashboard/Code_Development/Files/uynulntv_IAQ.csv"
+
+# Full path to your django project directory
+your_djangoproject_home="/~/Google\ Drive/Research/5_Sensors/Projects/UT2000-SummerDashboard/Dashboard_Development/"
+
+import sys, os
+sys.path.append(your_djangoproject_home) 
+os.environ['DJANGO_SETTINGS_MODULE'] = 'settings'
+
+from pm.models import PM
+import csv
+
+dataReader = csv.reader(open(csv_filepathname), delimiter=',', quotechar='"')
+for row in dataReader:
+	if row[0] != 'Month': 
+	# Ignore the header row, import everything else
+		pm = PM()
+		pm.concentrations = row[3]
+		pm.aqi = row[4]
+		pm.measurement_time = row[5]
+
+pm.save()
